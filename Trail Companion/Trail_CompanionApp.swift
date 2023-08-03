a530a408e5efc93ca2f3d9d6ed0923358321360d
Thomas Mary <ei.thomas.mary@gmail.com>
2023-08-03 15:34:02 +0200

    Initial Commit

---
+++ Trail Companion/Trail_CompanionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Trail_CompanionApp.swift
+//  Trail Companion
+//
+//  Created by Thomas Mary on 03/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct Trail_CompanionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

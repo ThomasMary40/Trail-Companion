d3d3c9b00cba39d2f4801bd71cfab5409d0e31af
Thomas Mary <ei.thomas.mary@gmail.com>
2023-09-23 09:25:06 +0200

    Gestion visuelle de la suppression et de la modification des traces dans la vue liste

--- Trail Companion/View/TraceList.swift
+++ Trail Companion/View/TraceList.swift
@@ -14,49 +14,62 @@ struct TraceList: View {
     var body: some View {
         VStack {
             NavigationView {
-                List(gpxList) { gpx in
-                    let viewModel = GPXViewModel(gpx: gpx)
-                    NavigationLink {
-                        TraceDetail(viewModel: viewModel)
-                    } label: {
-                        TraceRow(viewModel: viewModel)
+                List {
+                    ForEach(gpxList) { gpx in
+                        let viewModel = GPXViewModel(gpx: gpx)
+                        NavigationLink {
+                            TraceDetail(viewModel: viewModel)
+                        } label: {
+                            TraceRow(viewModel: viewModel)
+                        }
                     }
+                    .onDelete(perform: { indexSet in
+                        delete(at: indexSet)
+                    })
+                    .onMove(perform: { indices, newOffset in
+                        gpxList.move(fromOffsets: indices, toOffset: newOffset)
+                    })
                 }
                 .navigationTitle("Traces")
                 .toolbar {
-                    Button("+") {
-                        importing = true
-                    }
-                    .fileImporter(
-                        isPresented: $importing,
-                        allowedContentTypes: [.item]
-                    ) { result in
-                        switch result {
-                        case .success(let file):
-                            print(file.absoluteString)
-                            guard let url = URL(string: file.absoluteString) else {
-                                print("error")
-                                return
+                    ToolbarItemGroup {
+                        EditButton()
+                        Spacer()
+                        Button("+") {
+                            importing = true
+                        }
+                        .fileImporter(
+                            isPresented: $importing,
+                            allowedContentTypes: [.item]
+                        ) { result in
+                            switch result {
+                            case .success(let file):
+                                print(file.absoluteString)
+                                guard let url = URL(string: file.absoluteString) else {
+                                    print("error")
+                                    return
+                                }
+                                
+                                guard let importedGpx = addGpx(url) else {
+                                    print("error")
+                                    return
+                                }
+                                
+                                gpxList.append(importedGpx)
+                                
+                            case .failure(let error):
+                                print(error.localizedDescription)
                             }
-                            
-                            guard let importedGpx = addGpx(url) else {
-                                print("error")
-                                return
-                            }
-                            
-                            gpxList.append(importedGpx)
-                            
-                        case .failure(let error):
-                            print(error.localizedDescription)
                         }
                     }
+                    
                 }
             }
         }
     }
     
     func delete(at offsets: IndexSet) {
-        // delete the objects here
+        gpxList.remove(atOffsets: offsets)
     }
 }
 
